e4469cec011136d159eb75b6a8f89beb127f8a4b
kponky <ikponkedet@gmail.com>
2024-03-10 02:00:02 +0100

    scroll reveal added

--- main.js
+++ main.js
@@ -39,3 +39,57 @@ document.getElementById("navMenu").addEventListener("click", handleMenuFunction)
     backSpeed: 80,
     backDelay: 2000
 });
+
+// Wrap the initialization inside a function
+function initializeScrollReveal() {
+  const sr = ScrollReveal({
+    origin: "top",
+    distance: "100px",
+    duration: 2500,
+    reset: true
+  });
+
+  // Call reveal method for each element you want to animate
+  sr.reveal('.description-text-card', {});
+  sr.reveal('.description-name', { delay: 200 });
+  sr.reveal('.description-text-info', { delay: 200 });
+  sr.reveal('.description-text-btn', { delay: 200 });
+  sr.reveal('.description-image', { delay: 300 });
+  sr.reveal('.projects-wrapper', {interval: 200});
+  sr.reveal('.blog-wrapper', {interval: 300});
+  sr.reveal('.top-header', {});
+
+
+
+
+  const srLeft = ScrollReveal({
+    origin: "left",
+    distance: "100px",
+    duration: 2500,
+    reset: true
+  });
+
+  srLeft.reveal('.aboutMe-info', {delay: 200})
+  srLeft.reveal('.contact-info', {delay: 200})
+
+
+
+
+  const srRight = ScrollReveal({
+    origin: "right",
+    distance: "100px",
+    duration: 2500,
+    reset: true
+  });
+
+
+  srRight.reveal('.skills', {delay: 200})
+  srRight.reveal('.form-control', {delay: 200})
+
+}
+
+
+
+
+// Call the initialization function when the DOM is ready
+document.addEventListener('DOMContentLoaded', initializeScrollReveal);
